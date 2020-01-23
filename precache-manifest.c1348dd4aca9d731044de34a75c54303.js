b7edadc340ae3345cc4c4a46376ed355f9e5815a
Jing Wei <jingwei.chew@gmail.com>
2020-01-23 18:02:59 +0800

    Updates

---
+++ precache-manifest.c1348dd4aca9d731044de34a75c54303.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b6db7e3eaef8e3fe5cc6bc7033f330",
+    "url": "/image-processing-prototyper/index.html"
+  },
+  {
+    "revision": "f29a2c826ef6f1892b1c",
+    "url": "/image-processing-prototyper/static/css/main.eb78cb12.chunk.css"
+  },
+  {
+    "revision": "d6d845f3728fcdbc52d6",
+    "url": "/image-processing-prototyper/static/js/2.31b7cc97.chunk.js"
+  },
+  {
+    "revision": "d6ff9d976ff8601ec17eecab9b0d3005",
+    "url": "/image-processing-prototyper/static/js/2.31b7cc97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f29a2c826ef6f1892b1c",
+    "url": "/image-processing-prototyper/static/js/main.26f38c22.chunk.js"
+  },
+  {
+    "revision": "daca1e08e9959d8ab44b",
+    "url": "/image-processing-prototyper/static/js/runtime-main.f4f7a1c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/image-processing-prototyper/static/media/logo.5d5d9eef.svg"
+  }
+]);
